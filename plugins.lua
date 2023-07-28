63a65c4d195637dbfacd5e47211c5405783bb0cc
Marco Antônio <ma.souza.junior@gmail.com>
2023-07-27 21:28:41 -0300

    enh: update user settings

--- plugins.lua
+++ plugins.lua
@@ -3,38 +3,38 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
   -- Override plugin definition options
-    -- {
-    --   "neovim/nvim-lspconfig",
-    --   dependencies = {
-    --     -- format & linting
-    --     {
-    --       "jose-elias-alvarez/null-ls.nvim",
-    --       config = function()
-    --         require "custom.configs.null-ls"
-    --       end,
-    --     },
-    --   },
-    --   config = function()
-    --     require "plugins.configs.lspconfig"
-    --     require "custom.configs.lspconfig"
-    --   end, -- Override to setup mason-lspconfig
-    -- },
-    --
-    -- -- override plugin configs
-    -- {
-    --   "williamboman/mason.nvim",
-    --   opts = overrides.mason
-    -- },
-    --
-    -- {
-    --   "nvim-treesitter/nvim-treesitter",
-    --   opts = overrides.treesitter,
-    -- },
-    --
-    -- {
-    --   "nvim-tree/nvim-tree.lua",
-    --   opts = overrides.nvimtree,
-    -- },
+    {
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        -- format & linting
+        {
+          "jose-elias-alvarez/null-ls.nvim",
+          config = function()
+            require "custom.configs.null-ls"
+          end,
+        },
+      },
+      config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+      end, -- Override to setup mason-lspconfig
+    },
+
+    -- override plugin configs
+    {
+      "williamboman/mason.nvim",
+      opts = overrides.mason
+    },
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = overrides.treesitter,
+    },
+
+    {
+      "nvim-tree/nvim-tree.lua",
+      opts = overrides.nvimtree,
+    },
 
   -- Install a plugin
     {
@@ -69,8 +69,12 @@ local plugins = {
     },
     {
       "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
       config = function()
-        require'nvim-surround'.setup()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
       end
     },
     {
