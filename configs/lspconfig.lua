8caefeef1919a9f4c757d0738bd65db441ae170d
Marco Antônio <ma.souza.junior@gmail.com>
2023-08-04 16:04:11 -0300

    enh: avoid conflicting denols with tsserver

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,8 +3,23 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
+local function is_deno_project()
+  local files = vim.fs.find(
+    {'deno.json', 'deno.jsonc'},
+    { upward = true }
+  )
+  -- if table has something, return true
+  for _ in pairs(files) do return true end
+  return false
+end
+
+local ts_lsp = "denols"
+if not is_deno_project() then
+  ts_lsp = "tsserver"
+end
+
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", ts_lsp, "clangd", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
