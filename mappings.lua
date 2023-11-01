c7b5443403d9d806886ca75bcc64a5ba5d7ab0a0
Marco Antônio <ma.souza.junior@gmail.com>
2023-11-01 14:27:19 -0300

    fea: setup ollero

--- mappings.lua
+++ mappings.lua
@@ -1,33 +1,67 @@
 ---@type MappingsTable
 local M = {}
 
+local function oOpen()
+  require("ollero").open()
+end
+
+local function oTerm()
+  require("ollero").openTerm()
+end
+
 M.personal = {
   t = {
     -- ai
-    ["<leader>ac"] = { function() require("ollama").open() end, desc = "Toggle AI Chat" },
+    ["<M-a>"] = { oTerm, desc = "Toggle AI Chat" },
   },
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
 
     -- ai
-    ["<leader>ac"] = { function() require("ollama").open() end, desc = "Toggle AI Cahat" },
-    ["<leader>aa"] = { function() require("llm.completion").toggle_suggestion() end, desc = "Enable AI Assistent" },
+    ["<M-a>"] = { oTerm, desc = "Toggle AI Chat" },
+    ["<M-s>"] = { oOpen, desc = "Ask Llama" },
+    -- ["aa"] = { function() require("llm.completion").toggle_suggestion() end, desc = "Enable AI Assistent" },
 
     -- neogit
-    ["<leader>gg"] = { function() require("neogit").open() end, desc = "Manage Git (Neogit)" },
+    ["<leader>gg"] = {
+      function()
+        require("neogit").open()
+      end,
+      desc = "Manage Git (Neogit)",
+    },
 
-    -- lsp 
+    -- lsp
     ["gf"] = { vim.lsp.buf.format, desc = "Rename variable name" },
     ["gr"] = { vim.lsp.buf.rename, desc = "Rename variable name" },
     ["gd"] = { vim.lsp.buf.definition, desc = "Go to Definition" },
     ["gD"] = { vim.lsp.buf.references, desc = "Go to References" },
 
     -- harpoon
-    ['<leader>ml'] = { function() require("harpoon.ui").toggle_quick_menu() end },
-    ['<leader>mt'] = { function() require("harpoon.term").gotoTerminal() end },
-    ['<leader>mm'] = { function() require("harpoon.mark").add_file() end },
-    ['<leader>mn'] = { function() require("harpoon.ui").nav_next() end },
-    ['<leader>mb'] = { function() require("harpoon.ui").nav_prev() end },
+    ["<leader>ml"] = {
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+    },
+    ["<leader>mt"] = {
+      function()
+        require("harpoon.term").gotoTerminal()
+      end,
+    },
+    ["<leader>mm"] = {
+      function()
+        require("harpoon.mark").add_file()
+      end,
+    },
+    ["<leader>mn"] = {
+      function()
+        require("harpoon.ui").nav_next()
+      end,
+    },
+    ["<leader>mb"] = {
+      function()
+        require("harpoon.ui").nav_prev()
+      end,
+    },
   },
 }
 
--- plugins.lua
+++ plugins.lua
@@ -1,151 +1,151 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
   -- Override plugin definition options
-    {
-      "neovim/nvim-lspconfig",
-      dependencies = {
-        -- format & linting
-        {
-          "jose-elias-alvarez/null-ls.nvim",
-          config = function()
-            require "custom.configs.null-ls"
-          end,
-        },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
       },
-      config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.configs.lspconfig"
-      end, -- Override to setup mason-lspconfig
     },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
 
-    -- override plugin configs
-    {
-      "williamboman/mason.nvim",
-      opts = overrides.mason
-    },
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
 
-    {
-      "nvim-treesitter/nvim-treesitter",
-      opts = overrides.treesitter,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
 
-    {
-      "nvim-tree/nvim-tree.lua",
-      opts = overrides.nvimtree,
-    },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
 
   -- Install a plugin
-    {
-      "max397574/better-escape.nvim",
-      event = "InsertEnter",
-      config = function()
-        require("better_escape").setup()
-      end,
-    },
-    {
-      "windwp/nvim-ts-autotag",
-      event = "InsertEnter",
-      config = function()
-        require'nvim-ts-autotag'.setup()
-      end
-    },
-    {
-      "dkarter/bullets.vim",
-      event = "InsertEnter",
-      config = function()
-        vim.g.bullets_enabled_file_types = {
-          'markdown',
-          'text',
-          'gitcommit',
-          'scratch',
-        }
-      end
-    },
-    {
-      "TimUntersberger/neogit",
-      event = "InsertEnter",
-      config = function()
-        require'neogit'.setup()
-      end
-    },
-    {
-      "kylechui/nvim-surround",
-      event = "InsertEnter",
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      config = function()
-          require("nvim-surround").setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
-    },
-    {
-      "ThePrimeagen/harpoon",
-      event = "InsertEnter",
-      config = function()
-        require('telescope').load_extension('harpoon')
-      end
-    },
-    {
-      "wakatime/vim-wakatime",
-      event = 'VeryLazy',
-    },
-    {
-      "folke/todo-comments.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "dkarter/bullets.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.bullets_enabled_file_types = {
+        "markdown",
+        "text",
+        "gitcommit",
+        "scratch",
+      }
+    end,
+  },
+  {
+    "TimUntersberger/neogit",
+    event = "InsertEnter",
+    config = function()
+      require("neogit").setup()
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    event = "InsertEnter",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    event = "InsertEnter",
+    config = function()
+      require("telescope").load_extension "harpoon"
+    end,
+  },
+  {
+    "wakatime/vim-wakatime",
+    event = "VeryLazy",
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
   -- AI
-    -- {
-    --   "huggingface/llm.nvim",
-    --   event = "VeryLazy",
-    --   opts = {
-    --     model_eos = "<EOT>",
-    --     fim = {
-    --       enabled = true,
-    --       prefix = "<PRE> ",
-    --       middle = " <MID>",
-    --       suffix = " <SUF>",
-    --     },
-    --     model = "codellama/CodeLlama-13b-hf",
-    --     context_window = 4096,
-    --     -- model = "http://localhost:11434/api/",
-    --     -- tokenizer = {
-    --     --   repository = "codellama/CodeLlama-13b-hf",
-    --     -- }
-    --   },
-    -- },
-    {
-      "marco-souza/ollama.nvim",
-      name = "ollama",
-      event = "VeryLazy",
-      config = function ()
-        require('ollama').setup()
-      end
-    },
-    -- {
-    --   name = "ollama",
-    --   dir = "~/w/plugins/ollama.nvchad/",
-    --   event = "VeryLazy",
-    --   config = function ()
-    --     require('ollama').setup()
-    --   end
-    -- },
+  {
+    "marco-souza/ollero.nvim",
+    name = "ollero",
+    event = "VeryLazy",
+    config = function()
+      require("ollero").setup()
+    end,
+  },
+  -- {
+  --   "huggingface/llm.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     model_eos = "<EOT>",
+  --     fim = {
+  --       enabled = true,
+  --       prefix = "<PRE> ",
+  --       middle = " <MID>",
+  --       suffix = " <SUF>",
+  --     },
+  --     model = "codellama/CodeLlama-13b-hf",
+  --     context_window = 4096,
+  --     -- model = "http://localhost:11434/api/",
+  --     -- tokenizer = {
+  --     --   repository = "codellama/CodeLlama-13b-hf",
+  --     -- }
+  --   },
+  -- },
+  -- {
+  --   name = "ollama",
+  --   dir = "~/w/plugins/ollama.nvchad/",
+  --   event = "VeryLazy",
+  --   config = function ()
+  --     require('ollama').setup()
+  --   end
+  -- },
 
   -- To make a plugin not be loaded
-    -- {
-    --   "NvChad/nvim-colorizer.lua",
-    --   enabled = false
-    -- },
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
 
   -- All NvChad plugins are lazy-loaded by default
-    -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-    -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-    -- {
-    --   "mg979/vim-visual-multi",
-    --   lazy = false,
-    -- }
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
 }
 
 return plugins
