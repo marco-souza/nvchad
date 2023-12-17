d21210c0caee8741d4afcdc1bc2dfbbb36c4ddc3
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-17 16:56:22 -0300

    fea: update neogit

--- plugins.lua
+++ plugins.lua
@@ -64,11 +64,16 @@ local plugins = {
     end,
   },
   {
-    "TimUntersberger/neogit",
-    event = "InsertEnter",
-    config = function()
-      require("neogit").setup()
-    end,
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true
   },
   {
     "kylechui/nvim-surround",
